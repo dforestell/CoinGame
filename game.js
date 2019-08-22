4b6a6f5c658ec770cf24496f41f017f7c9b4eb9d
Dillon Forestell <dillons-mac@Dillons-MacBook-Pro.local>
2019-08-22 16:44:55 -0500

    add a lives var and decriment it when a poison is it, kill all remaining items upon winning the game or not having lives left and render a game over message

--- game.js
+++ game.js
@@ -9,28 +9,42 @@ let jumpButton;
 let text;
 let winningMessage;
 let won = false;
+let gameOver = false;
+let lives = 3;
 let currentScore = 0;
 let winningScore = 100;
 
 // add collectable items to the game
 function addItems() {
   items = game.add.physicsGroup();
-  createItem(375, 300, 'coin');
-  createItem(233, 444, 'coin');
-  createItem(0, 5, 'coin');
-  createItem(100, 100, 'coin');
-  createItem(650, 400, 'coin');
-  createItem(550, 100, 'coin');
-  createItem(450, 333, 'coin');
+  createItem(375, 400, 'coin');
+  createItem(575, 500, 'coin');
+  createItem(225, 500, 'coin');
+  createItem(100, 250, 'coin');
+  createItem(575, 150, 'coin');
+  createItem(525, 300, 'coin');
+  createItem(650, 250, 'coin');
+  createItem(225, 200, 'coin');
+  createItem(375, 100, 'poison');
+  createItem(370,500,'poison');
+  createItem(100, 375, 'poison');
+  createItem(125, 50, 'star');
 }
 
 // add platforms to the game
 function addPlatforms() {
   platforms = game.add.physicsGroup();
-  platforms.create(450, 500, 'platform');
-  platforms.create(50, 400, 'platform');
-  platforms.create(300, 255, 'platform2');
-  platforms.create(600, 280, 'platform2');
+  platforms.create(450, 550, 'platform');
+  platforms.create(100, 550, 'platform');
+  platforms.create(300, 450, 'platform');
+  platforms.create(250, 150, 'platform');
+  platforms.create(50, 300, 'platform');
+  platforms.create(150, 250, 'platform');
+  platforms.create(650, 300, 'platform');
+  platforms.create(550, 200, 'platform2');
+  platforms.create(300, 450, 'platform2');
+  platforms.create(400, 350, 'platform2');
+  platforms.create(100, 100, 'platform2');
   platforms.setAll('body.immovable', true);
 }
 
@@ -53,16 +67,30 @@ function createBadge() {
 function itemHandler(player, item) {
   console.log("hit this?")
   item.kill();
-  currentScore = currentScore + 15;
-  if (currentScore >= winningScore) {
+  if (item.key === 'coin'){
+    currentScore = currentScore + 10;
+    if (currentScore >= winningScore) {
       createBadge();
-  }
+    }
+  } else if (item.key === 'star'){
+    currentScore = currentScore + 25;
+    if (currentScore >= winningScore) {
+      createBadge();
+    }
+  } else if (item.key === 'poison'){
+    lives--
+    if (lives === 0){
+      gameOver = true;
+      items.callAll('kill');
+    }
+  } 
 }
 
 // when the player collects the badge at the end of the game
 function badgeHandler(player, badge) {
   badge.kill();
   won = true;
+  items.callAll('kill');
 }
 
 // setup game when the web page loads
@@ -81,6 +109,8 @@ window.onload = function () {
     game.load.spritesheet('player', 'chalkers.png', 48, 62);
     game.load.spritesheet('coin', 'coin.png', 36, 44);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
+    game.load.spritesheet('poison', 'poison.png', 32, 32);
+    game.load.spritesheet('star', 'star.png', 32, 32);
   }
 
   // initial game set up
@@ -104,7 +134,7 @@ window.onload = function () {
 
   // while the game is running
   function update() {
-    text.text = "SCORE: " + currentScore;
+    text.text = "SCORE: " + currentScore + "   LIVES REMAINING: " + lives;;
     game.physics.arcade.collide(player, platforms);
     game.physics.arcade.overlap(player, items, itemHandler);
     game.physics.arcade.overlap(player, badges, badgeHandler);
@@ -131,6 +161,9 @@ window.onload = function () {
       player.body.velocity.y = -400;
     }
     // when the player winw the game
+    if (gameOver) {
+      winningMessage.text = "YOU HAVE NO LIVES REMAINING"
+    }
     if (won) {
       winningMessage.text = "YOU WIN!!!";
     }
